2c002f0b93f70eb0f72f5e35a64f74c6b50fc1e0
berny13 <57494751+berny13@users.noreply.github.com>
2020-03-15 22:31:00 +0100

    Este archivo contiene todos los metodos de las funciones

--- Funciones.java
+++ Funciones.java
@@ -34,6 +34,28 @@ public class Funcion {
 		return sum;
 		
 	}
+	public double Funcion6(double [] num1) {
+
+        double [] num2 = new double [num1.length];
+        double z = 4;
+        double w = 4;
+
+
+        for (int cont = 0; cont < num1.length; cont++) {
+            if (num1[cont]%2==0) {
+                num2[cont] = num1[cont]*z; 
+                System.out.print("  Numero Par -->  " + num2[cont]);
+            }else {
+                num2[cont] = num1[cont]*w; 
+                System.out.print("  Numero Impar -->  " +num2[cont]);
+            }
+        }
+
+        
+		return z;
+
+
+    }
 
 }
 
