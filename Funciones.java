4f8da7b16ff581bd16f3830a94600f10f889de68
berny13 <57494751+berny13@users.noreply.github.com>
2020-03-15 22:29:38 +0100

    Este archivo contiene todos los metodos de las funciones

--- Funciones.java
+++ Funciones.java
@@ -1,5 +1,23 @@
 
 public class Funcion {
+	public Boolean Funcion1(int num) {
+		
+		Boolean resp = null;
+		
+		
+		int x = 7;
+		
+		if(num%x==0) {
+			
+			resp = true;
+			
+		}else {
+			resp = false;
+		}
+		
+		return resp;	
+		
+	}
 		
 	
 }
